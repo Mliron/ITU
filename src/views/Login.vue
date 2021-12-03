77a0f1e5084e4f175f4ae89d098e948dcc223e3d
Poggers Mahmudek <darkchaos007@gmail.com>
2021-12-03 15:58:22 +0100

    added router

---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <img src="fio.bmp" class="center">
+  <h1 align="center" class="title" >Přihlášení do aplikace Fio Broker</h1>
+  <form class="form">
+      <label for="name">Uživatelské jméno: </label><br>
+      <input type="text" id="fname" name="fname"><br>
+      <label for="pwd">Heslo:</label><br>
+    <input type="password" id="pwd" name="pwd"><br><br>
+    <input type="submit" value="Přihlásit se">
+  </form>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+//
+//export default {
+//  name: 'Home',
+//  components: {
+//    HelloWorld
+//  }
+//}
+</script>
