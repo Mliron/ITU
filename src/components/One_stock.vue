a86187d65696cc18acce5dc20897b2fe708d6cf8
Matus Fabo <matus.fabo@espressif.com>
2021-12-03 20:37:24 +0100

    added some portfolio functionality

---
+++ src/components/One_stock.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="one_stock" @click.left="onclick(data.symbol)">
+    <label v-for="(key, i) in data" :key="i">
+      {{key}}
+    </label>
+  </div>
+</template>
+
+<script>
+  export default{
+    props: ["onclick", "data"],
+    data(){
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  table { border-collapse: collapse; }
+  .one_stock { padding: 0.2em 0; }
+  .one_stock:hover { background-color: #666; }
+  .one_stock:hover label { color: #fff; }
+  label { display: inline-block; width: 150px; font-size: 16px;}
+</style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,3 +1,83 @@
 <template>
-    <h1>Fuck yall</h1>
+  <div class="portfolio">
+    <div>
+      <table>
+        <label v-for="(str, i) in pogger_keys" :key="i">
+          {{str}}
+        </label>
+        <tr v-for="(tru_pog, i) in poggers" :key="i">
+          <td>
+              <One_stock
+                :onclick="POGGERS"
+                :data="tru_pog"/>
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div class="graph" v-if="selected.shown">
+      {{selected.name}} <br>
+      <a href="https://ej2.syncfusion.com/vue/documentation/stock-chart/getting-started-vue3/">
+        Very pog graph
+      </a>
+    </div>
+  </div>
 </template>
+
+<script>
+  import One_stock from '@/components/One_stock.vue'  
+
+  export default{
+    components: {
+      One_stock
+    },
+    data(){
+      return{
+        pogger_keys: ["Symbol", "Change Rate", "Buy", "Sell"],
+        poggers:[
+          {
+            symbol : "Pog",
+            change : "0.666%",
+            buy    : "420",
+            sell   : "666"
+          },
+          {
+            symbol : "KEKW",
+            change : "0.42%",
+            buy    : "4542",
+            sell   : "4569"
+          },
+          {
+            symbol : "dogecoin",
+            change : "-1.58%",
+            buy    : "42069",
+            sell   : "50000"
+          },
+        ],
+        selected:{
+          name : "",
+          shown: false
+        }
+      }
+    },
+    methods:{
+      POGGERS(data){
+        console.log(data);
+        if(this.selected.name == data){
+          this.selected.shown = !this.selected.shown;
+        }
+        else{
+          this.selected.name = data; 
+          this.selected.shown = true;
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  table { border-collapse: collapse; }
+  td { border: 1px solid black; }
+  label { display: inline-block; width: 150px; font-size: 16px;}
+  div { float: left; }
+  .graph { width: 600px; height: 800px; border: 2px solid black; font-size: 32px; }
+</style>
