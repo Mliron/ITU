13cd60f6ea691983d08c6f61320cbc8974832d27
Matus Fabo <matus.fabo@espressif.com>
2021-12-04 09:08:28 +0100

    added tabs

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -31,20 +31,31 @@
         />
       </div><br>
       <div>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Statistics</th>
-              <th scope="col">Company info</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Name = eyyy</td>
-              <td>CEO cheated</td>
-            </tr>
-          </tbody>
-        </table>
+        <ul class="nav nav-tabs nav-justified">
+          <li class="nav-link fs-4" @click.prevent="set_active('company_information')">Statistics</li>
+          <li class="nav-link fs-4" @click.prevent="set_active('buy_sell')">Buy / Sell</li>
+          <li class="nav-link fs-4" @click.prevent="set_active('news')">News</li>
+        </ul>
+        <div class="tab-content py-3">
+          <div class="tab-pane fade" :class="{'active show': is_active('company_information')}" id="company_information">
+            <span class="tw-bold">Name:</span> VUT <br>
+            <span class="tw-bold">Website:</span> <a class="text-black" href="http://bitly.com/98K8eH">https://www.vut.cz/</a><br>
+            <span class="tw-bold">Industry:</span> Crushing people <br>
+            <span class="tw-bold">Name:</span> Soul sucking <br>
+          </div>
+          <div class="tab-pane fade" :class="{'active show': is_active('buy_sell')}" id="buy_sell">
+            <button onclick="buy()">Buy</button>
+            <button onclick="sell()">Sell</button>
+          </div>
+          <div class="tab-pane fade" :class="{'active show': is_active('news')}" id="news">
+            <ul>
+              <li><a class="text-black" href="https://i.pinimg.com/originals/86/41/d6/8641d6de409bfd8271b647db30229e6e.jpg">What happens when you go for a midnight snack.</a></li>
+              <li><a class="text-black" href="https://www.forbes.com/uk/advisor/wp-content/uploads/2021/05/short-coated-tan-puppy-stockpack-unsplash-scaled.jpg">I'm tired.. Enjoy this puppy</a></li>
+              <li><a class="text-black" href="https://www.cbc.ca/kidscbc2/content/the_feed/feathered-tree-viper.jpg">This snake looks nice</a></li>
+              <li><a class="text-black" href="https://www.youtube.com/watch?v=-ujIz6nmvsE">Advanced warfare tactics</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -68,7 +79,8 @@
           shown: false
         },
         noice:{
-        }
+        },
+        active_item: "company_information"
       }
     },
     methods:{
@@ -94,6 +106,12 @@
          .catch((error)=>{
             console.log(error);
           })
+      },
+      is_active (menuItem) {
+        return this.active_item === menuItem
+      },
+      set_active (menuItem) {
+        this.active_item = menuItem
       }
     },
     mounted(){
